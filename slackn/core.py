a2cbaa89660988817e06aa1767db49e5e21bd026
Bradley Cicenas <bradley.cicenas@gmail.com>
2015-11-09 00:55:57 -0500

    db update

--- slackn/core.py
+++ slackn/core.py
@@ -14,6 +14,14 @@ class SlacknDB(object):
     def _add_service(self, **kwargs):
         self.redis.hmset('services:' + uuid.uuid4(), kwargs)
 
+    def _get_hosts(self):
+        return [ self.redis.hgetall(k) for k in \
+                 self.redis.keys(pattern='hosts:*') ] 
+
+    def _get_services(self):
+        return [ self.redis.hgetall(k) for k in \
+                 self.redis.keys(pattern='services:*') ] 
+
     def _state_color(self, state):
         if state == 'CRITICAL':
             return 'red'
@@ -22,5 +30,3 @@ class SlacknDB(object):
         else:
             return 'green'
         
-    def _fetchall(self):
-        return self.redis.lrange('hosts', 0, -1)
